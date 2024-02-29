380b66e5698fe464f2770effc496d12662cf68a8
Vladimir Melnik <nexxonyou@gmail.com>
2024-02-29 23:18:49 +0300

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer light-blue accent-2">
+        <div className="footer-copyright">
+            <div className="container">
+                © {new Date().getFullYear()} Мельник В.Г. (ТКБО-02-22)
+                <a className="grey-text text-lighten-4 right" href="https://github.com/MidRangeShotCreator/ReactShop">Repository</a>
+            </div>
+        </div>
+    </footer>
+}
+
+export {Footer};
---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return <nav className="light-blue accent-2">
+        <div className="container">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">React Shop</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="https://github.com/MidRangeShotCreator/ReactShop">Repository</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+}
+
+export {Header};
